c69277e9d211cddbccedd0bc3fcc95fbf83375ff
zbilledo <156949555+zbilledo@users.noreply.github.com>
2024-01-30 19:47:02 +0000

    Finished

--- include/csc232.h
+++ include/csc232.h
@@ -18,9 +18,9 @@
 #define TRUE 1
 #define EXECUTE_BLOCK FALSE
 
-#define FINISHED_PART_1 FALSE
-#define FINISHED_PART_2 FALSE
-#define FINISHED_PART_3 FALSE
+#define FINISHED_PART_1 TRUE
+#define FINISHED_PART_2 TRUE
+#define FINISHED_PART_3 TRUE
 
 #include <algorithm>
 #include <cassert>
@@ -62,11 +62,37 @@ namespace csc232
 {
     // Add any user-defined functions prescribed in your assignment below
     // TODO: 1.1 Declare the Shape interface below
-
+class Shape{
+    public:
+    virtual double area()const=0;
+    virtual double perimeter()const=0;
+};
     // TODO: 2.1 Provide an inline definition of the Square class below
-
+class Square : public Shape {
+    public:
+    Square() : side{1.0} {}
+    double area() const override {
+        return side*side;
+    }
+    double perimeter() const override {
+        return side*4.0;
+    }
+    private:
+    double side;
+};
     // TODO: 3.1 Provide an inline definition of the Circle class below
-
+class Circle : public Shape {
+    public:
+    Circle() : radius{1.0} {}
+    double area() const override {
+        return M_PI*(radius*radius);
+    }
+    double perimeter() const override {
+        return 2*M_PI*radius;
+    }
+    private:
+    double radius;
+};
     // DO NOT Modify anything below this line
 } // namespace csc232
 
